ef65dc3179e24e2a38cf0c06cb9eeb4a340cf7a2
EC2 Default User <ec2-user@ip-172-31-69-99.ec2.internal>
2021-05-30 04:32:44 +0000

    Changes to be committed: 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#top'
+  root to: 'homes#top'
   get 'top' => 'homes#top'
   resources :books, except: :new
 end

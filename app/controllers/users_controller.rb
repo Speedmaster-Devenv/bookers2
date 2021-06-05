4519173a6484d256dc722529b4881108ffa1d447
EC2 Default User <ec2-user@ip-172-31-69-99.ec2.internal>
2021-06-05 01:19:28 +0000

    ユーザー情報の編集機能追加 Changes to be committed: 	modified:   app/controllers/users_controller.rb 	modified:   app/models/user.rb 	modified:   app/views/books/index.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/users/_show.html.erb 	new file:   app/views/users/edit.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20210605002944_add_note_to_users.rb 	modified:   db/schema.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  def edit
+    @user = User.find(current_user.id)
     #@post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:notice] = %q[You've updated user successfully.]
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :note)
   end
 end
